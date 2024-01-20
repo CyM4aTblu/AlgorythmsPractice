4a2639270d1f85ae15b857dc552f41b4e97e3ed9
CyM4aTblu <dimamik34@gmail.com>
2024-01-20 13:53:45 +0300

    getting prime nums

---
+++ SieveofEratosthenes.cpp
@@ -0,0 +1,39 @@
+﻿#include <vector>
+#include <iostream>
+using namespace std;
+vector<int> sieve(int n)
+{
+    if (n == 1)
+    {
+        return {1};
+    }
+    vector<bool> isprime(n, true);
+    vector<int> primeNumbers;
+    primeNumbers.push_back(2);
+    for (int i = 2; i < n; i++)
+    {
+        if (isprime[i])
+        {
+            for (int j = i * i; j < n; j += i)
+            {
+                isprime[j] = false;
+            }
+        }
+    }
+    for (int i = 3; i < isprime.size(); i++)
+    {
+        if (isprime[i])
+        {
+            primeNumbers.push_back(i);
+        }
+    }
+    return primeNumbers;
+}
+int main()
+{
+    vector<int> primes = sieve(200);
+    for (auto prime: primes)
+    {
+        cout << prime << " ";
+    }
+}
